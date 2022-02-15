4ea26c5729e12f4c80c411247cdc129ebc074ba3
Muhammad Nauman Raza <devraza@kalidoscope.dev.kaleidoscope.net>
2022-02-15 21:27:24 +0000

    Make setup better

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ with open("data/config.json", "a") as file:
     auth_token = input()
     file.write("{\n")
     file.write(f'   "user_token" : "{auth_token}",\n')
-    print("Now, input the preferred Channel ID for spamming and catching:")
+    print("Now, input the Channel ID of the channel for spamming and catching:")
     channel_id = input()
     file.write(f'   "channel_id" : "{channel_id}"\n')
     file.write("}")
 
-print("Setup completed. Please run the bot using the correct command:\nPlease see the GitHub repo for help on this.")
+print("Setup completed. Please run the bot using the correct command:\nPlease see the GitHub repo for help on this. However, please try:\npython3 main.py")
