6041fc07a98dc19b222b4d4dd6885c9bda941ab2
devraza <devraza@outlook.com>
2022-02-27 19:11:50 +0000

    Fix broken push

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
+import sys, subprocess
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
 with open("data/config.json", "a") as file:
-    print("Input your discord authorization token:")
+    install('discord.py-self')
+    print("\nInput your discord authorization token:")
     auth_token = input()
     file.write("{\n")
     file.write(f'   "user_token" : "{auth_token}",\n')
@@ -8,4 +14,4 @@ with open("data/config.json", "a") as file:
     file.write(f'   "channel_id" : "{channel_id}"\n')
     file.write("}")
 
-print("Setup completed. Please run the bot using the correct command:\nPlease see the GitHub repo for help on this. However, please try:\npython3 main.py")
+print("\n\nSetup completed. Please run the bot using the correct command:\nPlease see the GitHub repo for help on this. However, please try:\npython3 main.py")
